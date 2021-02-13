00fda8fcd4004a824062543a71e0dc3a53c81a68
Leonid Turchenkov <turchenkoff@gmail.com>
2021-02-14 01:33:23 +0700

    connection mock refactoring

--- connection_mock.go
+++ connection_mock.go
@@ -9,84 +9,84 @@ import (
 
 type ConnectionMock struct {
     mock.Mock
+    Err     error
+    RowsAff int64
 }
 
-func (db *ConnectionMock) Create(value interface{}) Connection {
-    args := db.Called(value)
+func (c *ConnectionMock) Create(value interface{}) Connection {
+    args := c.Called(value)
     return args.Get(0).(Connection)
 }
 
-func (db *ConnectionMock) Save(value interface{}) Connection {
-    args := db.Called(value)
+func (c *ConnectionMock) Save(value interface{}) Connection {
+    args := c.Called(value)
     return args.Get(0).(Connection)
 }
 
-func (db *ConnectionMock) First(dest interface{}, conds ...interface{}) Connection {
-    args := db.Called(dest, conds)
+func (c *ConnectionMock) First(dest interface{}, conds ...interface{}) Connection {
+    args := c.Called(dest, conds)
     return args.Get(0).(Connection)
 }
 
-func (db *ConnectionMock) Last(dest interface{}, conds ...interface{}) Connection {
-    args := db.Called(dest, conds)
+func (c *ConnectionMock) Last(dest interface{}, conds ...interface{}) Connection {
+    args := c.Called(dest, conds)
     return args.Get(0).(Connection)
 }
 
-func (db *ConnectionMock) Find(dest interface{}, conds ...interface{}) Connection {
-    args := db.Called(dest, conds)
+func (c *ConnectionMock) Find(dest interface{}, conds ...interface{}) Connection {
+    args := c.Called(dest, conds)
     return args.Get(0).(Connection)
 }
 
-func (db *ConnectionMock) Limit(limit int) Connection {
-    args := db.Called(limit)
+func (c *ConnectionMock) Limit(limit int) Connection {
+    args := c.Called(limit)
     return args.Get(0).(Connection)
 }
 
-func (db *ConnectionMock) Offset(offset int) Connection {
-    args := db.Called(offset)
+func (c *ConnectionMock) Offset(offset int) Connection {
+    args := c.Called(offset)
     return args.Get(0).(Connection)
 }
 
-func (db *ConnectionMock) Delete(value interface{}, conds ...interface{}) Connection {
-    args := db.Called(value, conds)
+func (c *ConnectionMock) Delete(value interface{}, conds ...interface{}) Connection {
+    args := c.Called(value, conds)
     return args.Get(0).(Connection)
 }
 
-func (db *ConnectionMock) Count(count *int64) Connection {
-    args := db.Called(count)
+func (c *ConnectionMock) Count(count *int64) Connection {
+    args := c.Called(count)
     return args.Get(0).(Connection)
 }
 
-func (db *ConnectionMock) Transaction(fc func(tx *gorm.DB) error, opts ...*sql.TxOptions) (err error) {
-    args := db.Called(fc, opts)
+func (c *ConnectionMock) Transaction(fc func(tx *gorm.DB) error, opts ...*sql.TxOptions) (err error) {
+    args := c.Called(fc, opts)
     return args.Error(0)
 }
 
-func (db *ConnectionMock) Begin(opts ...*sql.TxOptions) Connection {
-    args := db.Called(opts)
+func (c *ConnectionMock) Begin(opts ...*sql.TxOptions) Connection {
+    args := c.Called(opts)
     return args.Get(0).(Connection)
 }
 
-func (db *ConnectionMock) Commit() Connection {
-    args := db.Called()
+func (c *ConnectionMock) Commit() Connection {
+    args := c.Called()
     return args.Get(0).(Connection)
 }
 
-func (db *ConnectionMock) Rollback() Connection {
-    args := db.Called()
+func (c *ConnectionMock) Rollback() Connection {
+    args := c.Called()
     return args.Get(0).(Connection)
 }
 
-func (db *ConnectionMock) Exec(sql string, values ...interface{}) Connection {
-    args := db.Called(sql, values)
+func (c *ConnectionMock) Exec(sql string, values ...interface{}) Connection {
+    args := c.Called(sql, values)
     return args.Get(0).(Connection)
 }
 
-func (db *ConnectionMock) Error() error {
-    args := db.Called()
-    return args.Error(0)
+func (c *ConnectionMock) Error() error {
+    return c.Err
 }
 
-func (db *ConnectionMock) RowsAffected() int64 {
-    args := db.Called()
-    return int64(args.Int(0))
+func (c *ConnectionMock) RowsAffected() int64 {
+    return c.RowsAff
 }
